5169353d7b70e2d14b522809809a14386b4cdfbd
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-04-02 11:32:54 +0530

    done

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,17 @@
-const Item = ({ item }) => {
+
+
+const Item = ({ item, onDelete }) => {
 
     // Render a single item
     // Add a Delete and Edit button
-    return null;
+    return(
+        <div>
+            <h3>{item.name}</h3>
+            <div>
+                <button onClick={() => onDelete(item.id)}>Delete</button>
+            </div>
+        </div>
+    )
 };
 
 export default Item;
